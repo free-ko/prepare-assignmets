36b58947c5b297e4535d8f7e02e4de080c31c0e7
Kay <duddnr654@naver.com>
2023-04-28 16:53:10 +0900

    [FEAT]: Routes 설정(Home)

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,7 @@ import React from 'react';
 import { Router } from '~/router';
 
 const App = () => {
-  return (
-    <>
-      <Router />
-    </>
-  );
+  return <Router />;
 };
 
 export default App;
